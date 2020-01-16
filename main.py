a75d0a01c01ea7f71a1ce7324ce4a6acee7d5b13
= <fkt.kohei@gmail.com>
2020-01-16 13:11:20 +0900

    add wrong case for test

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ def count_carbons(smiles):
     cnt += smiles.count("C")
     cnt += smiles.count("c")
     return cnt
+
+
+def wrong_count_nitorgens(smiles):
+    cnt = 0
+    cnt = smiles.count("N")
+    cnt = smiles.count("n")
+    return cnt
+
--- test.py
+++ test.py
@@ -4,8 +4,20 @@ import main
 
 def test_count_carbons():
     glucose = "OC[C@@H](O1)[C@@H](O)[C@H](O)[C@@H](O)[C@@H](O)"
+    nicotine = "CN1CCC[C@H]1c2cccnc2"
     carbons_in_glucose = main.count_carbons(glucose)
+    carbons_in_nicotine = main.count_carbons(nicotine)
     assert carbons_in_glucose == 6
+    assert carbons_in_nicotine == 10
+
+
+def test_count_nitrogens():
+    glucose = "OC[C@@H](O1)[C@@H](O)[C@H](O)[C@@H](O)[C@@H](O)"
+    nicotine = "CN1CCC[C@H]1c2cccnc2"
+    nitrogens_in_glucose = main.count_nitrogens(glucose)
+    nitrogens_in_nicotine = main.count_nitrogens(nicotine)
+    assert nitrogens_in_glucose == 0
+    assert nitrogens_in_nicotine == 2
 
 
 if __name__ == "__main__":
